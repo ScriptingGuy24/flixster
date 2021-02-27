5a0769edc13a03505a25f9ea507b0694b90a6dc5
Geoffrey Stribling <striblig@oregonstate.edu>
2021-02-27 01:57:57 -0500

    Flixster Part 2 completed!

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Geoffrey Stribling on 2/27/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
